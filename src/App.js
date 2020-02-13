6e97bb15d5c77dc96b0d7b13de247c62cddf0d34
BhSabeur <bhsabeur@gmail.com>
2020-02-13 14:43:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Timer from "./components/Timer";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+        <Timer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
